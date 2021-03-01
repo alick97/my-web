6fd60ddbc10f2492c4b8ce9776e48b613e2dcc76
alick97 <alick97@outlook.com>
2021-03-01 17:11:04 +0800

    vue: Rendering a list of vue components.

--- vue_tutorial/moz-todo-vue/src/App.vue
+++ vue_tutorial/moz-todo-vue/src/App.vue
@@ -4,8 +4,9 @@
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <h1>To-Do List</h1>
     <ul>
-      <li><to-do-item label="My ToDo Item" :done="true"></to-do-item></li>
-      <li><to-do-item label="My ToDo Item" :done="true"></to-do-item></li>
+      <li v-for="item in ToDoItems" :key="item.id">
+          <to-do-item :id="item.id" :label="item.label" :done="item.done"></to-do-item>  
+      </li>
     </ul>
   </div>
 </template>
@@ -13,12 +14,23 @@
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 import ToDoItem from './components/ToDoItem.vue'
+import uniqueId from 'lodash.uniqueid'
 
 export default {
   name: 'App',
   components: {
     // HelloWorld
     ToDoItem
+  },
+  data() {
+    return {
+      ToDoItems: [
+        { id: uniqueId('todo-'), label: 'Learn Vue', done: false },
+        { id: uniqueId('todo-'), label: 'Create a Vue project with the CLI', done: true },
+        { id: uniqueId('todo-'), label: 'Have fun', done: true },
+        { id: uniqueId('todo-'), label: 'Create a to-do list', done: false }
+      ]
+    }
   }
 }
 </script>
