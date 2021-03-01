6833ded10fc982458783288ef3482f3a502a6a82
alick97 <alick97@outlook.com>
2021-03-01 16:47:57 +0800

    vue: Creating a ToDoItem component.

--- vue_tutorial/moz-todo-vue/src/App.vue
+++ vue_tutorial/moz-todo-vue/src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <h1>To-Do List</h1>
+    <ul>
+      <li><to-do-item label="My ToDo Item" :done="true"></to-do-item></li>
+      <li><to-do-item label="My ToDo Item" :done="true"></to-do-item></li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import ToDoItem from './components/ToDoItem.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+    ToDoItem
   }
 }
 </script>
