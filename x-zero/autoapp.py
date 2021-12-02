0d9e5766945e555f6ae83ea7c49c86d92dc1442c
alick97 <alick97@outlook.com>
2021-12-03 01:05:24 +0800

    re generator x-zero base from cookiecutter-flask

---
+++ x-zero/autoapp.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+"""Create an application instance."""
+from x_zero.app import create_app
+
+app = create_app()
--- x-zero/tests/test_x_zero.py
+++
@@ -1,5 +0,0 @@
-from x_zero import __version__
-
-
-def test_version():
-    assert __version__ == '0.1.0'
