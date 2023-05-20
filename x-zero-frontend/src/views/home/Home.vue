dc589f3415743c025d7d6c71f97c2b3e9178b1d6
alick97 <alick97@outlook.com>
2023-05-20 23:50:22 +0800

    frontend: add tailwind

--- x-zero-frontend/src/views/home/Home.vue
+++ x-zero-frontend/src/views/home/Home.vue
@@ -3,7 +3,7 @@ import { RouterLink } from 'vue-router';
 
 </script>
 <template>
-  <h1>X-ZERO</h1>
+  <h1 class="text-4xl font-bold underline">X-ZERO</h1>
   <div>
     <RouterLink to="/login">Sign in</RouterLink>
   </div>
